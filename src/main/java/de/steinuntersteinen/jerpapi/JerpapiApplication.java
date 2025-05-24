b6d1e4c506a4a8ab908dbbe06a3e1c431971f61c
Julien Zigan <zigan.julien@gmail.com>
2025-05-24 14:12:49 +0200

    initial commit

---
+++ src/main/java/de/steinuntersteinen/jerpapi/JerpapiApplication.java
@@ -0,0 +1,13 @@
+package de.steinuntersteinen.jerpapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JerpapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JerpapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/steinuntersteinen/jerpapi/JerpapiApplicationTests.java
@@ -0,0 +1,13 @@
+package de.steinuntersteinen.jerpapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JerpapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
